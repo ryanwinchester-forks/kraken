05a07f5e410d01249a86d663650aa3a717deb5d6
Ryan Winchester <fungku@users.noreply.github.com>
2014-09-02 10:03:42 -0700

    Update User.php

--- src/Users/User.php
+++ src/Users/User.php
@@ -1,12 +1,11 @@
 <?php namespace Kraken;
 
 use Illuminate\Auth\UserTrait;
-use Illuminate\Database\Eloquent\Model;
 use Illuminate\Auth\Reminders\RemindableTrait;
 use Illuminate\Contracts\Auth\User as UserContract;
 use Illuminate\Contracts\Auth\Remindable as RemindableContract;
 
-class User extends Model implements UserContract, RemindableContract {
+class User extends BaseModel implements UserContract, RemindableContract {
 
 	use UserTrait, RemindableTrait;
 
