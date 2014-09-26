ac1faa75696647118e94bcb94fa1e620e0d2b3a0
Ryan Winchester <fungku@users.noreply.github.com>
2014-09-26 11:52:52 -0700

    Update CommandResponse.php

--- src/Commands/CommandResponse.php
+++ src/Commands/CommandResponse.php
@@ -2,11 +2,42 @@
 
 class CommandResponse {
 
+    protected $status;
+    
     protected $message;
 
+    public function status()
+    {
+        return $this->status;
+    }
+    
     public function message()
     {
         return $this->message;
+    }
+    
+    public function success($message)
+    {
+        $this->status = "success";
+        $this->message = $message;
+    }
+    
+    public function error($message)
+    {
+        $this->status = "danger";
+        $this->message = $message;
+    }
+    
+    public function warning($message)
+    {
+        $this->status = "warning";
+        $this->message = $message;
+    }
+    
+    public function info($message)
+    {
+        $this->status = "info";
+        $this->message = $message;
     }
 
 } 
