c5f6c4dceab7197cc45b881e06ae7d2adeaf9765
fungku <fungku@gmail.com>
2014-10-24 18:44:26 -0700

    HttpClient

---
+++ src/Contracts/Http/Client.php
@@ -0,0 +1,19 @@
+<?php namespace Kraken\Contracts\Http;
+
+interface Client {
+
+    public function get($url, array $options);
+
+    public function post($url, array $options);
+
+    public function head($url, array $options);
+
+    public function put($url, array $options);
+
+    public function patch($url, array $options);
+
+    public function delete($url, array $options);
+
+    public function options($url, array $options);
+
+}
