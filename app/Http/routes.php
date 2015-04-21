2a1109d7246e3ca856c05a4c0cddaf04d5198af2
fungku <fungku@gmail.com>
2015-04-20 17:20:35 -0700

    clean up routes

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -1,18 +1,38 @@
 <?php
 
+// ----------------------------------------------------------------------------
+// Dashboard
+// ----------------------------------------------------------------------------
 Route::get('/', 'HomeController@index');
+Route::resource('forms', 'FormsController', [
+    'only' => ['index', 'show', 'create', 'edit']
+]);
 
-Route::resource('forms', 'FormsController', ['only' => ['index', 'show', 'create' , 'edit']]);
-
+// ----------------------------------------------------------------------------
+// API
+// ----------------------------------------------------------------------------
 Route::group(['prefix' => 'api'], function () {
-    Route::resource('contacts', 'Api\ContactsController', ['except' => ['create', 'edit']]);
-    Route::resource('properties', 'Api\PropertiesController', ['except' => ['create', 'edit']]);
-    Route::resource('property-types', 'Api\PropertyTypesController', ['except' => ['create', 'edit']]);
-    Route::resource('tags', 'Api\TagsController', ['except' => ['create', 'edit']]);
-    Route::resource('forms', 'Api\FormsController', ['except' => ['create', 'edit']]);
+    Route::resource('contacts', 'Api\ContactsController', [
+        'except' => ['create', 'edit']
+    ]);
+    Route::resource('properties', 'Api\PropertiesController', [
+        'except' => ['create', 'edit']
+    ]);
+    Route::resource('property-types', 'Api\PropertyTypesController', [
+        'except' => ['create', 'edit']
+    ]);
+    Route::resource('tags', 'Api\TagsController', [
+        'except' => ['create', 'edit']
+    ]);
+    Route::resource('forms', 'Api\FormsController', [
+        'except' => ['create', 'edit']
+    ]);
 });
 
+// ----------------------------------------------------------------------------
+// Controllers
+// ----------------------------------------------------------------------------
 Route::controllers([
-    'auth' => 'Auth\AuthController',
+    'auth'     => 'Auth\AuthController',
     'password' => 'Auth\PasswordController',
 ]);
